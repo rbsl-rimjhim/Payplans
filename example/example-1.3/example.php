225a6d30ad41e0f690fa641b64958f5787997239
Shyam Verma <shyam@readybytes.in>
2011-11-20 14:24:20 +0530

    Updated plugin and app

--- example/example-1.3/example.php
+++ example/example-1.3/example.php
@@ -4,24 +4,25 @@
 * @copyright	Copyright (C) 2009 - 2009 Ready Bytes Software Labs Pvt. Ltd. All rights reserved.
 * @license	GNU/GPL, see LICENSE.php
 * @package	Payplans
-* @subpackage	Alert Pay Payment App
 * @contact	payplans@readybytes.in
 */
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 /**
- * Payplans Alert Pay Plugin
- * @author Gaurav
+ * Extend plugin from XiPlugin
  */
-class plgPayplansAlertpay extends XiPlugin
+class plgPayplansExample extends XiPlugin
 {
+	// Tell payplans that you have some apps, which need to be shown in system
 	public function onPayplansSystemStart()
 	{
-		//add discount app path to app loader
-		$appPath = dirname(__FILE__).DS.'alertpay'.DS.'app';
+		//add your example-app path to app loader
+		$appPath = dirname(__FILE__).DS.'example'.DS.'app';
 		PayplansHelperApp::addAppsPath($appPath);
 
+		// theme can also be loaded here
+		
 		return true;
 	}
 }
